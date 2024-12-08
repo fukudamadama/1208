eb1fcde918b000cb0b199d7632008772a3999d07
fukudama <i.a.m.v.0210@gmail.com>
2024-12-08 15:59:19 +0900

    株価アプリをUP

--- main.py
+++ main.py
@@ -1,157 +1,55 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-st.title('Streamlit 基礎をやってみる')# タイトル
-
-df = pd.DataFrame({
-    '1列目': [1, 2, 3, 4],
-    '2列目': [10, 20, 30, 40]
-}) # データフレームの作成
-
-st.write(df)  # データフレームの表示
-st.dataframe(df.style.highlight_max(axis=0), width=100, height=100) # データフレームの表示（スタイル付き）
-
-#公式ドキュメントを見に行くと、いろんな表示形式があるので確認する（display data）
-
-st.table(df.style.highlight_max(axis=0)) # データフレームの表示（スタイル付き）
-
-"""
-# 章
-## 節
-### 項
-
-```python
-import streamlit as st
-import numpy as np
-import pandas as pd
-```
-"""
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-st.title('Streamlit Hello World') # タイトル
-
-st.write('DataFrame') # テキストの表示
-
-df = pd.DataFrame(
-    np.random.rand(20, 3),
-    columns=['a', 'b', 'c']
-)   # データフレームの作成
-
-st.line_chart(df) # 折れ線グラフの表示
-st.area_chart(df) # エリアグラフの表示
-
-df = pd.DataFrame(
-    np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
-    columns=['lat', 'lon']
-)
-
-st.map(df) # 地図の表示
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
-
-st.title('Streamlit Hello World') # タイトル
-
-st.write('Display Image') #画像の表示
-
-img = Image.open('pic/img031.jpg') # 画像の読み込み
-st.image(img, caption='sample', use_column_width=True) # 画像の表示
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
-
-st.title('Streamlit Hello World') # タイトル
-
-st.write('Interactive Widgets') # ウィジェットの表示
-
-text = st.sidebar.text_input('あなたの趣味を教えてください。') # テキスト入力
-'あなたの趣味は', text, 'です。' # テキスト表示
-
-condition = st.sidebar.slider('あなたの今の調子は？', 0, 100, 50) # スライダー
-'コンディション：', condition # テキスト表示
-
 import streamlit as st # フロントエンドを扱うstreamlitの機能をインポート
-import time # 時間を扱う機能をインポート
-
-st.title("streamlitの基礎") # タイトルが出力される
-st.write("hello world") # hello worldが出力される
-
-# レイアウトとして２列を定義
-col1, col2 = st.columns(2)
-
-# 1列目をwithで囲む
-with col1:
-    st.write("列1がここに表示されます")
-
-# 2列目をwithで囲む
-with col2:
-    st.write("列2がここに表示されます")
-
-
-
-st.sidebar.write("hello world") #.sidebar付けるとサイトバーに出力されます。
-st.text_input("ここに文字が入力できます。") # テキストを入力できます。
-
-slider_text = st.slider("スライダーで数字を決定できます。",0,100,5) # (最小、最大値、デフォルト値)の順で設定されます。
-"スライダーの数字:",slider_text
-
-st.button("ボタン") # ボタンが設置されます。
-
-x = st.empty() # 文字が出力される場所をあらかじめ確保します。その場所をxとしています。
-bar = st.progress(0) # 進捗0のプログレスバーを出力します。
-
-# iに0から99まで代入しながら実行されます
-for i in range(100):
-    time.sleep(0.1) # 0.1秒待機します。
-    x.text(i) # 確保した場所xに代入されたiの値を代入します。
-    bar.progress(i) # 進捗iに変更します。
-    i += 1 # iに1足し算して代入します。
-
-# 選択肢を配列で指定して選択肢を出力します。
-st.selectbox("選んでください。",["選択肢1","選択肢2","選択肢3"])
-
-
-
-# ダウンロードする文字を定義し、output_textに代入します。
-output_text = "この文字がダウンロードされます"
-
- # 代入された文字をダウンロードするボタンを設置。オプションは内容をdataに指定、ファイル名をfile_nameに指定、ファイルタイプをmimeに指定
-st.download_button(label='記事内容 Download', 
-                   data=output_text, 
-                   file_name='out_put.txt',
-                   mime='text/plain',
-                   )
-
-
-# ファイルアップローダーを設置します。typeでアップロードできるファイルの種類を指定できます。
-file_upload = st.file_uploader("ここに音声認識したファイルをアップロードしてください。",type=["png","jpg"])
-
-# ファイルがアップロードされた時にその画像を表示します。
-if (file_upload !=None):
-    st.image(file_upload)# 画像を表示します。
-
-
-
-import numpy as np # 数列を扱う機能をインポート
+import requests # リクエストするための機能をインポート
+from datetime import datetime # 現在時刻などの時間を扱う機能をインポート
 import pandas as pd # データフレームを扱う機能をインポート
 
-# 乱数の配列を作るメソッドを作ります。引数r,cとし、それぞれおのデフォルト値を10と5に設定します。
-def rand_df(r=10, c=5):
-    df = pd.DataFrame(
-        np.random.randn(r, c),
-        columns=('col %d' % i for i in range(c)))# 乱数10の５個の数列を作ります。カラムの設定は0-4の名前を付けます。
-    return df # 作ったデータフレームを返します。
+# 選択肢を作成
+city_code_list = {
+    "東京都":"130010",
+    "大阪" : "270000",
+}
+# 選択肢のデフォルトを設定
+city_code_index = "東京都"
 
-dataframe = rand_df(r=10,c=3) # rに10、cに3を代入したrand_dfメソッドを処理します。
 
-# 表の描画します。
-st.dataframe(dataframe.head(n=3))
-# データフレームのチャートの描画します。
-st.line_chart(dataframe)
+st.title("天気アプリ") # タイトル
+st.write("調べたい地域を選んでください。") # サブタイトル
+city_code_index = st.selectbox("地域を選んでください。",city_code_list.keys()) # 選択肢のキーをst.selectboxで選択し、city_code_indexに代入
+city_code = city_code_list[city_code_index] # 選択したキーからAPIのリクエストに使うcityコードに変換し、city_codeに代入
+current_city_code = st.empty() # 選択中の地域を補油時するための箱をcurrent_city_codeとして用意
+current_city_code.write("選択中の地域:" + city_code_index) # 用意した箱に選択肢した地域を代入し、表示させる
+
+url = "https://weather.tsukumijima.net/api/forecast/city/" + city_code # APIにリクエストするURLを作成
+
+
+response = requests.get(url) # 作成したリクエスト用URLでアクセスして、responseに代入
+
+weather_json = response.json() # responseにjson形式の天気のデータが返ってくるので、response.json()をweather_jsonに代入
+now_hour = datetime.now().hour # 現在の天気情報取得のために、現在時刻の時間をnow_hourに代入
+
+# 今日の天気はweather_json['forecasts'][0]['chanceOfRain']
+# 明日の天気はweather_json['forecasts'][1]['chanceOfRain']
+# 明後日の天気はweather_json['forecasts'][2]['chanceOfRain']
+# にそれぞれ格納されている
+
+# 天気の情報を0-6時、6-12時、12-18時、18-24時の4つに分けて降水確率を今日、明日、明後日の３日間の天気を返すため、場合分けする。
+if 0 <= now_hour and now_hour < 6:
+    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T00_06'] # 今日の0-6時の降水確率を取得し、weather_nowに代入
+elif 6 <= now_hour and now_hour < 12:
+    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T06_12'] # 今日の6-12時の降水確率を取得し、weather_nowに代入
+elif 12 <= now_hour and now_hour < 18:
+    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T12_18'] # 今日の12-18時の降水確率を取得し、weather_nowに代入
+else:
+    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T18_24'] # 今日の18-24時の降水確率を取得し、weather_nowに代入
+
+# 現在時刻の降水確率をweather_now_textに代入
+weather_now_text = "現在の降水確率 : " + weather_now
+st.write(weather_now_text) # 現在時刻の降水確率を表示
+
+# 今日、明日、明後日の降水確率をDadaFrameに代入
+df1 = pd.DataFrame(weather_json['forecasts'][0]['chanceOfRain'],index=["今日"]) # index名を今日という文字列に設定
+df2 = pd.DataFrame(weather_json['forecasts'][1]['chanceOfRain'],index=["明日"]) # index名を明日という文字列に設定
+df3 = pd.DataFrame(weather_json['forecasts'][2]['chanceOfRain'],index=["明後日"]) # index名を明後日という文字列に設定
+
+df = pd.concat([df1,df2,df3]) # 今日、明日、明後日の降水確率を結合して一覧にしてdfに代入
+st.dataframe(df) # 一覧にした降水確率を表示
